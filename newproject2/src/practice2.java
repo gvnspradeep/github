4014c1199b148aaeacdf8e5e856ac5f0285125f6
gvnspradeep <guduru.pradeep224@gmail.com>
2023-03-06 18:14:49 -0800

    06/03/2023. Added all arithmatic methods

---
+++ newproject2/src/practice2.java
@@ -0,0 +1,32 @@
+public class practice2 {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println(getSum(2, 3));
+        System.out.println(getDiff(2, 3));
+        System.out.println(getProduct(2,3));
+        System.out.println(getQuotient(4,2));
+    }
+
+    public static int getSum(int a, int b) {
+        int sum = (a + b);
+        return sum;
+    }
+
+    public static int getDiff(int a, int b) {
+        int diff;
+        if (a > b) {
+            diff = (a - b);
+        } else {
+            diff = (b - a);
+        }
+        return diff;
+    }
+    public static int getProduct(int c,int d){
+        int product=(c*d);
+        return product;
+    }
+    public static int getQuotient(int e,int f){
+        int quotient=(e/f);
+        return quotient;
+    }
+}
