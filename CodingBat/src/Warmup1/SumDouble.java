82cbcb37a557a1311c0cb0cdb143fd54dfc5189e
gvnspradeep <guduru.pradeep224@gmail.com>
2023-03-09 17:53:52 -0800

    09/03/2023. sumdouble

---
+++ CodingBat/src/Warmup1/SumDouble.java
@@ -0,0 +1,17 @@
+package Warmup1;
+
+public class SumDouble {
+    public static void main(String[] args){
+        checkSumDouble(2,3);
+    }
+    public static void checkSumDouble(int a,int b){
+        if(a==b){
+            int sum=2*(a+b);
+            System.out.println(sum);
+        }
+        else if(a!=b){
+            int sum=a+b;
+            System.out.println(sum);
+        }
+    }
+}
