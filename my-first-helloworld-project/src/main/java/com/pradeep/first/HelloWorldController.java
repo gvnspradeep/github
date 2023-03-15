aec3aab4576816ee34957f0c5ee1b0440cd48131
guduru.pradeep224 <guduru.pradeep224@gmail.com>
2023-03-14 17:59:46 -0700

    my first springboot

---
+++ my-first-helloworld-project/src/main/java/com/pradeep/first/HelloWorldController.java
@@ -0,0 +1,11 @@
+package com.pradeep.first;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello World";
+    }
+}
---
+++ my-first-helloworld-project/src/main/java/com/pradeep/first/MyFirstHelloworldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pradeep.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstHelloworldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstHelloworldProjectApplication.class, args);
+	}
+
+}
